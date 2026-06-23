c5dede10a8ca57e5407d8724e2e9607322ae65b0
HlengiweNcube <hlehlencube123@gmail.com>
2026-06-23 17:44:20 +0100

    fix: configure DEBUG and ALLOWED_HOSTS from env

--- hvcs_project/settings.py
+++ hvcs_project/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-7*-kxlqnk$cf6&inw(_1ldt*@w4*+i2aix+!&90s*cms3bulyj')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
