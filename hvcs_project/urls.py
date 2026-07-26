3d1c1e39504fa3e402f0269d3662d9102b00c8c3
HlengiweNcube <hlehlencube123@gmail.com>
2026-07-26 17:16:08 +0100

    Remove Django admin: clear admin registrations and URL

--- hvcs_project/urls.py
+++ hvcs_project/urls.py
@@ -1,12 +1,10 @@
 
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from accounts import views as account_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('dashboard/', account_views.dashboard, name='dashboard'),
     path('', auth_views.LoginView.as_view(
